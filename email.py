757f0d0ec6b4b40c51d67dd7a7d035c0d6587a43
Kevin W Palmer <kevinwpalmer@gmail.com>
2012-08-15 22:52:51 -0400

    It can access the server.  Now I need to slect an inbox, search and fetch

--- email.py
+++ email.py
@@ -1,4 +1,8 @@
 import getpass, imaplib
 
-M=imaplib.IMAP4('imap.gmail.com', '993')
-M.login(getpass.getpass())
+M=imaplib.IMAP4_SSL('imap.gmail.com')
+
+M.login('kevinwpalmer', "jsfern251634")
+#IMAP4.select(readonly)
+
+#Next do select,search, and fetch.
